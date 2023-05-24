9b193f5c2f8e13358f5e8fee387455b85ee99efd
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-05-24 10:37:06 +0900

    バグっていた部分を全体的に修正

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,9 @@ fn mul(a: Expr, b: Expr) -> Expr { Expr::Mul(Box::new(a), Box::new(b)) }
 fn div(a: Expr, b: Expr) -> Expr { Expr::Div(Box::new(a), Box::new(b)) }
 fn num(val: i32) -> Expr { Expr::Num(val) }
 
-
-mod Rpn {
-    use Expr;
+// TODO: Module 化
+//mod Rpn {
+//    use Expr;
     enum RpnOp {
         Num(i32),
         Add,
@@ -46,55 +46,65 @@ mod Rpn {
         Mul,
         Div,
     }
-    fn tokenize_num(mut i: &usize, text: &str) -> RpnOp { // TODO: Result 型が本来は適切
-        let val = 0i32;
-        while let Some(u) = text[*i].to_digit(10) {
-            val += u as i32; // TODO: i32 に収まらない場合の処理
-            *i += 1;
+    fn tokenize_num(i: &mut usize, chars: &Vec<char>) -> RpnOp { // TODO: Result 型が本来は適切
+        let mut val = 0i32;
+        while *i < chars.len() {
+            match chars[*i] {
+                c @ '0' ..= '9' => {
+                    val = val * 10 + c.to_digit(10).unwrap() as i32; // TODO: i32 に収まらない場合の対応
+                    *i += 1;
+                },
+                _ => { *i -= 1; break; },
+            }
         }
         RpnOp::Num(val)
     }
-    fn tokenize(text: String) -> Vec<RpnOp> {
+    fn tokenize(text: &str) -> Vec<RpnOp> {
         let mut i = 0usize;
         let mut ops = vec![];
-        while i < text.len() {
-            let c = text[i];
-            let op = match c {
-                ' ' => continue,
-                '+' => RpnOp::Add,
-                '-' => RpnOp::Sub,
-                '*' => RpnOp::Mul,
-                '/' => RpnOp::Div,
-                _ => tokenize_num(&mut i, &text)
+        let chars = text.chars().collect::<Vec<_>>(); 
+        while i < chars.len() {
+            let c = chars[i];
+            let u = match c {
+                ' ' => None,
+                '+' => Some(RpnOp::Add),
+                '-' => Some(RpnOp::Sub),
+                '*' => Some(RpnOp::Mul),
+                '/' => Some(RpnOp::Div),
+                _ =>   Some(tokenize_num(&mut i, &chars))
             };
-            ops.push(op);
+            match u {
+                Some(op) => ops.push(op),
+                None => {},
+            }
             i += 1;
         }
         ops
     }
-    fn build_ast<F>(&mut exprs: Vec<Expr>, op: F)
+    fn build_ast<F>(exprs: &mut Vec<Expr>, op: F)
         where F: Fn(Expr, Expr) -> Expr {
-        let a = exprs.pop.unwrap();
-        let b = exprs.pop.unwrap();
+        let a = exprs.pop().unwrap();
+        let b = exprs.pop().unwrap();
         exprs.push(op(a, b));
     }
-    fn parse(text: String) -> Expr {
-        let exprs = vec![];
+    fn parse(text: &str) -> Expr {
+        let mut exprs = vec![];
         for token in tokenize(text) {
             match token {
-                Num(i) => exprs.push(num(i)),
-                Add => { build_ast(&mut exprs, add) },
-                Sub => { build_ast(&mut exprs, sub) },
-                Div => { build_ast(&mut exprs, div) },
-                Mul => { build_ast(&mut exprs, mul) },
+                RpnOp::Num(i) => exprs.push(num(i)),
+                RpnOp::Add => { build_ast(&mut exprs, add) },
+                RpnOp::Sub => { build_ast(&mut exprs, sub) },
+                RpnOp::Div => { build_ast(&mut exprs, div) },
+                RpnOp::Mul => { build_ast(&mut exprs, mul) },
             }
         }
         exprs.pop().unwrap() // TODO: stack のチェック
     }
-}
+//}
 
 fn main() {
-    let arith = add(num(1), mul(sub(num(10), num(4)), div(num(6), num(3))));
+    let arith = parse("3 6 / 1 4 - * 10 +");
+    //let arith = parse("3 4 + 1 2 - *");
     println!("{} = {}", arith.to_string(), arith.eval());
 }
 
