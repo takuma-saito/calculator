64fa952b9431d6fabddd74ab5812bde6f7cf510f
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-05-26 00:08:52 +0900

    型の柔軟性をもたせた

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 
 use std::fmt;
-use std::ops;
+use std::ops::{Add, Sub, Mul, Div};
 
-enum Expr {
-    NumI64(i64),
-    Add(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    Div(Box<Expr>, Box<Expr>),
+enum Expr<T: NumOp + fmt::Display> {
+    NumI64(T),
+    Add(Box<Expr<T>>, Box<Expr<T>>),
+    Sub(Box<Expr<T>>, Box<Expr<T>>),
+    Mul(Box<Expr<T>>, Box<Expr<T>>),
+    Div(Box<Expr<T>>, Box<Expr<T>>),
 }
 
 struct Faction {
@@ -15,8 +15,8 @@ struct Faction {
     den: i64,
 }
 
-impl Expr {
-    fn eval(self) -> i64 {
+impl<T: NumOp + fmt::Display> Expr<T> {
+    fn eval(self) -> T {
         match self {
             Expr::NumI64(val) => val,
             Expr::Add(a, b) => (*a).eval() + (*b).eval(),
@@ -27,7 +27,7 @@ impl Expr {
     }
 }
 
-impl fmt::Display for Expr {
+impl<T: NumOp + fmt::Display> fmt::Display for Expr<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Expr::NumI64(a) => write!(f, "{}", a),
@@ -39,39 +39,41 @@ impl fmt::Display for Expr {
     }
 }
 
-impl ops::Add<Expr> for Expr {
-    type Output = Expr;
+trait NumOp<T = Self, Output = Self>:
+    Add<T, Output = Output>
+    + Sub<T, Output = Output>
+    + Mul<T, Output = Output>
+    + Div<T, Output = Output> {}
 
-    fn add(self, other: Expr) -> Expr {
+impl<T: NumOp + fmt::Display> Add for Expr<T> {
+    type Output = Expr<T>;
+    fn add(self, other: Expr<T>) -> Expr<T> {
         Expr::Add(Box::new(self), Box::new(other))
     }
 }
-
-impl ops::Sub<Expr> for Expr {
-    type Output = Expr;
-
-    fn sub(self, other: Expr) -> Expr {
+impl<T: NumOp + fmt::Display> Sub for Expr<T> {
+    type Output = Expr<T>;
+    fn sub(self, other: Expr<T>) -> Expr<T> {
         Expr::Sub(Box::new(self), Box::new(other))
     }
 }
-
-impl ops::Mul<Expr> for Expr {
-    type Output = Expr;
-
-    fn mul(self, other: Expr) -> Expr {
+impl<T: NumOp + fmt::Display> Mul for Expr<T> {
+    type Output = Expr<T>;
+    fn mul(self, other: Expr<T>) -> Expr<T> {
         Expr::Mul(Box::new(self), Box::new(other))
     }
 }
-
-impl ops::Div<Expr> for Expr {
-    type Output = Expr;
-    
-    fn div(self, other: Expr) -> Expr {
+impl<T: NumOp + fmt::Display> Div for Expr<T> {
+    type Output = Expr<T>;
+    fn div(self, other: Expr<T>) -> Expr<T> {
         Expr::Div(Box::new(self), Box::new(other))
     }
 }
 
-fn num(val: i64) -> Expr { Expr::NumI64(val) }
+impl<T: NumOp + fmt::Display> NumOp for Expr<T> {}
+impl NumOp for i64 {}
+
+fn num(val: i64) -> Expr<i64> { Expr::NumI64(val) }
 
 enum RpnOp {
     NumI64(i64),
@@ -96,13 +98,13 @@ fn tokenize(text: &str) -> Vec<RpnOp> {
     }
     ops
 }
-fn build_ast<F>(exprs: &mut Vec<Expr>, op: F)
-    where F: FnOnce(Expr, Expr) -> Expr {
+fn build_ast<F>(exprs: &mut Vec<Expr<i64>>, op: F)
+    where F: FnOnce(Expr<i64>, Expr<i64>) -> Expr<i64> {
     let a = exprs.pop().unwrap();
     let b = exprs.pop().unwrap();
     exprs.push(op(a, b));
 }
-fn parse(text: &str) -> Expr {
+fn parse(text: &str) -> Expr<i64> {
     let mut exprs = vec![];
     for token in tokenize(text) {
         match token {
@@ -120,13 +122,13 @@ fn parse(text: &str) -> Expr {
 fn test() {
     let mut arith = parse("1 2 +");
     assert_eq!("(2 + 1)", format!("{}", arith));
-    assert_eq!(3, arith.eval());
+    assert_eq!(3i64, arith.eval());
     arith = parse("3 4 + 1 2 - *");
     assert_eq!("((2 - 1) * (4 + 3))", format!("{}", arith));
-    assert_eq!(7, arith.eval());
+    assert_eq!(7i64, arith.eval());
     arith = parse("3 6 / 1 4 - * 10 +");
     assert_eq!("(10 + ((4 - 1) * (6 / 3)))", format!("{}", arith));
-    assert_eq!(16, arith.eval());
+    assert_eq!(16i64, arith.eval());
 }
 
 fn main() {
