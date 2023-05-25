228c0469688d1c099fe1351a5c0e2099b8ab2fc9
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-05-25 10:09:24 +0900

    モジュール化を断念

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+mod
+
 enum Expr {
     Num(i32),
     Add(Box<Expr>, Box<Expr>),
@@ -36,71 +38,67 @@ fn mul(a: Expr, b: Expr) -> Expr { Expr::Mul(Box::new(a), Box::new(b)) }
 fn div(a: Expr, b: Expr) -> Expr { Expr::Div(Box::new(a), Box::new(b)) }
 fn num(val: i32) -> Expr { Expr::Num(val) }
 
-// TODO: Module 化
-//mod Rpn {
-//    use Expr;
-    enum RpnOp {
-        Num(i32),
-        Add,
-        Sub,
-        Mul,
-        Div,
-    }
-    fn tokenize_num(i: &mut usize, chars: &Vec<char>) -> RpnOp { // TODO: Result 型が本来は適切
-        let mut val = 0i32;
-        while *i < chars.len() {
-            match chars[*i] {
-                c @ '0' ..= '9' => {
-                    val = val * 10 + c.to_digit(10).unwrap() as i32; // TODO: i32 に収まらない場合の対応
-                    *i += 1;
-                },
-                _ => { *i -= 1; break; },
-            }
+enum RpnOp {
+    Num(i32),
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+fn tokenize_num(i: &mut usize, chars: &Vec<char>) -> RpnOp { // TODO: Result 型が本来は適切
+    let mut val = 0i32;
+    while *i < chars.len() {
+        match chars[*i] {
+            c @ '0' ..= '9' => {
+                val = val * 10 + c.to_digit(10).unwrap() as i32; // TODO: i32 に収まらない場合の対応
+                *i += 1;
+            },
+            _ => { *i -= 1; break; },
         }
-        RpnOp::Num(val)
     }
-    fn tokenize(text: &str) -> Vec<RpnOp> {
-        let mut i = 0usize;
-        let mut ops = vec![];
-        let chars = text.chars().collect::<Vec<_>>(); 
-        while i < chars.len() {
-            let c = chars[i];
-            let u = match c {
-                ' ' => None,
-                '+' => Some(RpnOp::Add),
-                '-' => Some(RpnOp::Sub),
-                '*' => Some(RpnOp::Mul),
-                '/' => Some(RpnOp::Div),
-                _ =>   Some(tokenize_num(&mut i, &chars))
-            };
-            match u {
-                Some(op) => ops.push(op),
-                None => {},
-            }
-            i += 1;
+    RpnOp::Num(val)
+}
+fn tokenize(text: &str) -> Vec<RpnOp> {
+    let mut i = 0usize;
+    let mut ops = vec![];
+    let chars = text.chars().collect::<Vec<_>>(); 
+    while i < chars.len() {
+        let c = chars[i];
+        let u = match c {
+            ' ' => None,
+            '+' => Some(RpnOp::Add),
+            '-' => Some(RpnOp::Sub),
+            '*' => Some(RpnOp::Mul),
+            '/' => Some(RpnOp::Div),
+            _ =>   Some(tokenize_num(&mut i, &chars))
+        };
+        match u {
+            Some(op) => ops.push(op),
+            None => {},
         }
-        ops
+        i += 1;
     }
-    fn build_ast<F>(exprs: &mut Vec<Expr>, op: F)
-        where F: Fn(Expr, Expr) -> Expr {
-        let a = exprs.pop().unwrap();
-        let b = exprs.pop().unwrap();
-        exprs.push(op(a, b));
-    }
-    fn parse(text: &str) -> Expr {
-        let mut exprs = vec![];
-        for token in tokenize(text) {
-            match token {
-                RpnOp::Num(i) => exprs.push(num(i)),
-                RpnOp::Add => { build_ast(&mut exprs, add) },
-                RpnOp::Sub => { build_ast(&mut exprs, sub) },
-                RpnOp::Div => { build_ast(&mut exprs, div) },
-                RpnOp::Mul => { build_ast(&mut exprs, mul) },
-            }
+    ops
+}
+fn build_ast<F>(exprs: &mut Vec<Expr>, op: F)
+    where F: Fn(Expr, Expr) -> Expr {
+    let a = exprs.pop().unwrap();
+    let b = exprs.pop().unwrap();
+    exprs.push(op(a, b));
+}
+fn parse(text: &str) -> Expr {
+    let mut exprs = vec![];
+    for token in tokenize(text) {
+        match token {
+            RpnOp::Num(i) => exprs.push(num(i)),
+            RpnOp::Add => { build_ast(&mut exprs, add) },
+            RpnOp::Sub => { build_ast(&mut exprs, sub) },
+            RpnOp::Div => { build_ast(&mut exprs, div) },
+            RpnOp::Mul => { build_ast(&mut exprs, mul) },
         }
-        exprs.pop().unwrap() // TODO: stack のチェック
     }
-//}
+    exprs.pop().unwrap() // TODO: stack のチェック
+}
 
 fn main() {
     let arith = parse("3 6 / 1 4 - * 10 +");
