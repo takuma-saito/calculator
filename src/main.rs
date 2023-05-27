bd94cf8f1491d2e6392e51dee1b6160218230f73
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-05-27 12:40:23 +0900

    primitive 型を外に出した

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,105 @@
 use std::fmt;
 use std::ops::{Add, Sub, Mul, Div};
 
-enum Expr<T: NumOp> {
-    NumI64(T),
-    Add(Box<Expr<T>>, Box<Expr<T>>),
-    Sub(Box<Expr<T>>, Box<Expr<T>>),
-    Mul(Box<Expr<T>>, Box<Expr<T>>),
-    Div(Box<Expr<T>>, Box<Expr<T>>),
+enum Expr {
+    Number(Number),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
 }
 
-struct Faction {
-    num: i64,
-    den: i64,
+enum Number {
+    I64(i64),
+    I32(i32),
 }
 
-impl<T: NumOp> Expr<T> {
-    fn eval(self) -> T {
-        match self {
-            Expr::NumI64(val) => val,
-            Expr::Add(a, b) => (*a).eval() + (*b).eval(),
-            Expr::Sub(a, b) => (*a).eval() - (*b).eval(),
-            Expr::Mul(a, b) => (*a).eval() * (*b).eval(),
-            Expr::Div(a, b) => (*a).eval() / (*b).eval(),
+impl Number {
+    fn get_i64(&self) -> Option<i64> {
+        if let Number::I64(val) = &self { Some(*val) } else { None }
+    }
+}
+
+impl Sub for Number {
+    type Output = Number;
+    fn sub(self, other: Number) -> Number {
+        match (self, other) {
+            (Number::I64(x), Number::I64(y)) => Number::I64(x - y),
+            (Number::I64(x), Number::I32(y)) => Number::I64(x - y as i64),
+            (Number::I32(x), Number::I64(y)) => Number::I64(x as i64 - y),
+            (Number::I32(x), Number::I32(y)) => Number::I32(x - y),
         }
     }
 }
 
-impl<T: NumOp> fmt::Display for Expr<T> {
+impl Add for Number {
+    type Output = Number;
+    fn add(self, other: Number) -> Number {
+        match (self, other) {
+            (Number::I64(x), Number::I64(y)) => Number::I64(x + y),
+            (Number::I64(x), Number::I32(y)) => Number::I64(x + y as i64),
+            (Number::I32(x), Number::I64(y)) => Number::I64(x as i64 + y),
+            (Number::I32(x), Number::I32(y)) => Number::I32(x + y),
+        }
+    }
+}
+
+impl Mul for Number {
+    type Output = Number;
+    fn mul(self, other: Number) -> Number {
+        match (self, other) {
+            (Number::I64(x), Number::I64(y)) => Number::I64(x * y),
+            (Number::I64(x), Number::I32(y)) => Number::I64(x * y as i64),
+            (Number::I32(x), Number::I64(y)) => Number::I64(x as i64 * y),
+            (Number::I32(x), Number::I32(y)) => Number::I32(x * y),
+        }
+    }
+}
+
+impl Div for Number {
+    type Output = Number;
+    fn div(self, other: Number) -> Number {
+        match (self, other) {
+            (Number::I64(x), Number::I64(y)) => Number::I64(x / y),
+            (Number::I64(x), Number::I32(y)) => Number::I64(x / y as i64),
+            (Number::I32(x), Number::I64(y)) => Number::I64(x as i64 / y),
+            (Number::I32(x), Number::I32(y)) => Number::I32(x / y),
+        }
+    }
+}
+
+impl fmt::Display for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Expr::NumI64(a) => write!(f, "{}", a),
+            Number::I64(val) => write!(f, "{}", val),
+            Number::I32(val) => write!(f, "{}", val),
+        }
+    }
+}
+
+trait NumOps<T = Self, Output = Self>:
+    Add<T, Output = Output>
+    + Sub<T, Output = Output>
+    + Mul<T, Output = Output>
+    + Div<T, Output = Output>
+    + fmt::Display {}
+
+impl Expr {
+    fn eval(self) -> Number {
+        match self {
+            Expr::Number(number) => number,
+            Expr::Add(a, b) => Add::add((*a).eval(), (*b).eval()),
+            Expr::Sub(a, b) => Sub::sub((*a).eval(), (*b).eval()),
+            Expr::Mul(a, b) => Mul::mul((*a).eval(), (*b).eval()),
+            Expr::Div(a, b) => Div::div((*a).eval(), (*b).eval()),
+        }
+    }
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Number(num) => write!(f, "{}", num),
             Expr::Add(ref a, ref b) => write!(f, "({} + {})", *a, *b),
             Expr::Sub(ref a, ref b) => write!(f, "({} - {})", *a, *b),
             Expr::Mul(ref a, ref b) => write!(f, "({} * {})", *a, *b),
@@ -39,45 +109,37 @@ impl<T: NumOp> fmt::Display for Expr<T> {
     }
 }
 
-trait NumOp<T = Self, Output = Self>:
-    Add<T, Output = Output>
-    + Sub<T, Output = Output>
-    + Mul<T, Output = Output>
-    + Div<T, Output = Output>
-    + fmt::Display {}
-
-impl<T: NumOp> Add for Expr<T> {
-    type Output = Expr<T>;
-    fn add(self, other: Expr<T>) -> Expr<T> {
+impl Add for Expr {
+    type Output = Expr;
+    fn add(self, other: Expr) -> Expr {
         Expr::Add(Box::new(self), Box::new(other))
     }
 }
-impl<T: NumOp> Sub for Expr<T> {
-    type Output = Expr<T>;
-    fn sub(self, other: Expr<T>) -> Expr<T> {
+impl Sub for Expr {
+    type Output = Expr;
+    fn sub(self, other: Expr) -> Expr {
         Expr::Sub(Box::new(self), Box::new(other))
     }
 }
-impl<T: NumOp> Mul for Expr<T> {
-    type Output = Expr<T>;
-    fn mul(self, other: Expr<T>) -> Expr<T> {
+impl Mul for Expr {
+    type Output = Expr;
+    fn mul(self, other: Expr) -> Expr {
         Expr::Mul(Box::new(self), Box::new(other))
     }
 }
-impl<T: NumOp> Div for Expr<T> {
-    type Output = Expr<T>;
-    fn div(self, other: Expr<T>) -> Expr<T> {
+impl Div for Expr {
+    type Output = Expr;
+    fn div(self, other: Expr) -> Expr {
         Expr::Div(Box::new(self), Box::new(other))
     }
 }
 
-impl<T: NumOp> NumOp for Expr<T> {}
-impl NumOp for i64 {}
-
-fn num(val: i64) -> Expr<i64> { Expr::NumI64(val) }
+impl NumOps for Expr {}
+impl NumOps for Number {}
+impl NumOps for i64 {}
 
 enum RpnOp {
-    NumI64(i64),
+    Number(Number),
     Add,
     Sub,
     Mul,
@@ -93,23 +155,23 @@ fn tokenize(text: &str) -> Vec<RpnOp> {
             "-" => RpnOp::Sub,
             "*" => RpnOp::Mul,
             "/" => RpnOp::Div,
-            _ =>   RpnOp::NumI64(token.parse::<i64>().unwrap()),
+            _ =>   RpnOp::Number(Number::I64(token.parse::<i64>().unwrap())),
         };
         ops.push(rpn_op);
     }
     ops
 }
-fn build_ast<F>(exprs: &mut Vec<Expr<i64>>, op: F)
-    where F: FnOnce(Expr<i64>, Expr<i64>) -> Expr<i64> {
+fn build_ast<F>(exprs: &mut Vec<Expr>, op: F)
+    where F: FnOnce(Expr, Expr) -> Expr {
     let a = exprs.pop().unwrap();
     let b = exprs.pop().unwrap();
     exprs.push(op(a, b));
 }
-fn parse(text: &str) -> Expr<i64> {
+fn parse(text: &str) -> Expr {
     let mut exprs = vec![];
     for token in tokenize(text) {
         match token {
-            RpnOp::NumI64(i) => exprs.push(num(i)),
+            RpnOp::Number(num) => exprs.push(Expr::Number(num)),
             RpnOp::Add => { build_ast(&mut exprs, |a, b| a + b) },
             RpnOp::Sub => { build_ast(&mut exprs, |a, b| a - b) },
             RpnOp::Div => { build_ast(&mut exprs, |a, b| a / b) },
@@ -119,17 +181,21 @@ fn parse(text: &str) -> Expr<i64> {
     exprs.pop().unwrap() // TODO: stack のチェック
 }
 
+fn eval_and_get_i64(arith: Expr) -> i64 {
+    arith.eval().get_i64().unwrap()
+}
+
 #[test]
 fn test() {
     let mut arith = parse("1 2 +");
     assert_eq!("(2 + 1)", format!("{}", arith));
-    assert_eq!(3i64, arith.eval());
+    assert_eq!(3i64, eval_and_get_i64(arith));
     arith = parse("3 4 + 1 2 - *");
     assert_eq!("((2 - 1) * (4 + 3))", format!("{}", arith));
-    assert_eq!(7i64, arith.eval());
+    assert_eq!(7i64, eval_and_get_i64(arith));
     arith = parse("3 6 / 1 4 - * 10 +");
     assert_eq!("(10 + ((4 - 1) * (6 / 3)))", format!("{}", arith));
-    assert_eq!(16i64, arith.eval());
+    assert_eq!(16i64, eval_and_get_i64(arith));
 }
 
 fn main() {
